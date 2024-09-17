57151151ac732876031fcd17208e1400a7defa8b
taf <muzirat0@gmail.com>
2024-09-17 03:50:03 +0200

    error handlers

--- backend/app.py
+++ backend/app.py
@@ -28,13 +28,24 @@ if Config.SEASE_ENV == "test":
     from config.helpers import init_categories
     init_categories()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({ "Page not found", 404 })
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return jsonify({ "Method not allowed", 405 })
+
 @app.route('/')
 def home():
     return jsonify('Welcome to ShopEase API')
 
 @app.route('/SE/img/<filename>', methods=['GET'])
 def serve_image(filename):
-    return send_from_directory(Config.UPLOAD_FOLDER, filename)
+    try:
+        return send_from_directory(Config.UPLOAD_FOLDER, filename)
+    except:
+        return jsonify({"message": "Image not found!"}), 404
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")

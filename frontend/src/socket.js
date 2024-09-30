59e526c985a57acd4695d69406c5828a0c815a98
tAf <muzirat0@gmail.com>
2024-10-01 01:00:09 +0200

    Dynamic socket server url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,5 @@
 import { io } from 'socket.io-client';
+import { BASE_URL } from './utils/api';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:5000';
 
-export const socket = io(URL);
+export const socket = io(BASE_URL);

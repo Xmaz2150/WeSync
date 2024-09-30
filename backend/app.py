424a155afa0046d5af9da1c9ecd752104e142fc5
tAf <muzirat0@gmail.com>
2024-09-30 14:51:08 +0200

    Backend: WebSockets 0.0.0
    - WeBsocket server logic

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,8 @@ from config.settings import Config
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS, cross_origin
 
+from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
 CORS(app,
@@ -20,7 +22,10 @@ app.register_blueprint(user_views)
 app.config.from_object('config.settings.Config')
 
 jwt = JWTManager(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
+
+""" Pre request User Look up """
 @jwt.user_identity_loader
 def user_identity_lookup(user):
     return user.id
@@ -30,6 +35,8 @@ def user_lookup_callback(_jwt_header, jwt_data):
     identity = jwt_data["sub"]
     return storage.get(User, id=identity)
 
+
+""" Custom Error handlers"""
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({ "Page not found": 404 }), 404
@@ -42,6 +49,8 @@ def method_not_allowed(e):
 def method_not_allowed(e):
     return jsonify({ "Method not allowed": 405 }), 405
 
+
+""" Home Route and image server """
 @app.route('/')
 def home():
     return jsonify({"message": "Welcome to WeSync API"})
@@ -53,5 +62,20 @@ def serve_image(filename):
     except:
         return jsonify({"message": "Image not found!"}), 404
 
+
+""" WebSockets"""
+@socketio.on('connect')
+def on_connect():
+    print('Someone connected!')
+
+@socketio.on('disconnect')
+def on_disconnect():
+    print('Someone disconnected!')
+
+@socketio.on('create-something')
+def on_create_something(data):
+    emit('foo', data, broadcast=True)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)

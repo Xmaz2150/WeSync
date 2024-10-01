f80d07f8753955962d8c4f7512fc6b90c6dbf2c1
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2024-10-01 19:43:41 +0200

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,4 +2,4 @@ import { io } from 'socket.io-client';
 
 const WEBSOCKET_SERVER_URL = process.env.REACT_APP_WS_SERVER;
 
-export const socket = io(`${WEBSOCKET_SERVER_URL}:5000`);
+export const socket = io(`${WEBSOCKET_SERVER_URL}`);
